a4f20ee9427de60584365da85ccf4e439fb1c8c0
D3monEmper0r <D3monEmper0r@outlook.de>
2021-03-21 16:37:01 +0100

    Initial commit

---
+++ configs/example-loadconfig.py
@@ -0,0 +1,30 @@
+##### Imports #####
+import yaml
+import os
+
+##### Define config file #####
+configFile = os.path.join('.', 'configs', 'config.yaml')
+
+##### Open file and load content #####
+def loader(filepath):
+    with open(filepath, 'r') as s:
+        data = yaml.safe_load(s)
+    return data
+
+##### Define vars and values for later imports #####
+if os.path.isfile(configFile):
+    try:
+        temp = loader(configFile)
+        token = temp['Bot']['token']
+    except ImportError:
+        print('Loadconfig, import ERROR: Bot token')
+    try:
+        temp = loader(configFile)
+        prefix = temp['Bot']['prefix']
+    except ImportError:
+        print('Loadconfig, import ERROR: Bot prefix')
+    try:
+        temp = loader(configFile)
+        serverId = temp['Bot']['serverid']
+    except ImportError:
+        print('Loadconfig, import ERROR: Server ID')
